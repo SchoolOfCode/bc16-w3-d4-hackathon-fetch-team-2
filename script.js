18389956534a45f9e93fb90ddafdb4593eb9598b
mrw46 <mrw46@outlook.com>
2024-02-23 09:40:15 +0000

    function to fetch data written

--- script.js
+++ script.js
@@ -1,21 +1,31 @@
-const url = "https://icanhazdadjoke.com"
+//Endpoint for the API which gets hourly temp & precipitation for London
+const url = "https://api.open-meteo.com/v1/forecast?latitude=51.5085&longitude=-0.1257&hourly=temperature_2m,precipitation_probability,precipitation"
 
-async function fetchJoke() {
+// Define fetch function
+async function fetchData() {
 
-const response = await fetch(url, {
-  headers: {
-    Accept: "application/json", 
-  },
-});
+  //Declare variable which awaits fetch resolution
+  const response = await fetch(url);
 
-if (!response.ok) {
-   console.error(response.status);
-   console.error(response.text());
-   return;
+  //Log error to console if response from server not ok
+  if (!response.ok) {
+    console.error(response.status);
+    console.error(response.text());
+    return;
+  }
+
+  //Covert json to object and cache as variable
+  const data = await response.json();
+
+  //Log object to console
+  console.log(data);
+
 }
-const data = await response.json();
 
-console.log(data.joke);
+
+
+
+/*
 const jokeElement = document.getElementById("joke");
 jokeElement.textContent = data.joke;
 
@@ -26,4 +36,4 @@ console.log = function() {
   originalLog.apply(console, arguments);
   jokeHistory.innerHTML += `${data.joke}<br>`;
 };
-}
+*/
